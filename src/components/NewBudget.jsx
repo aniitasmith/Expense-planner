c935526af90cbeb59acc7699fc6b5e7eced1221a
Ana <ana.smith@itglobers.com>
2023-11-20 23:38:39 -0500

    conectar budget con api

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
+import axios from "axios"
 import Message from "./Message"
+
+const API_BASE_URL = 'http://localhost:3000/api/v1'
 
 const NewBudget = ({
   budget,
@@ -8,6 +11,9 @@ const NewBudget = ({
   setIsValidBudget
 }) => {
   const [message, setMessage] = useState('')
+  const [budgetName, setbudgetName] =useState('')
+  const [isRecurring, setIsRecurring] =useState('')
+
 
   const handleBudget = (e) => {
     e.preventDefault()
@@ -16,13 +22,42 @@ const NewBudget = ({
       setMessage('Its not a valid budget')
       return
     }
+    if(!budgetName || budgetName < '') {
+      setMessage('Its not a valid budget name')
+      return
+    }
     setMessage('')
+    addBudget({
+      name: budgetName,
+      amount: Number(budget),
+      recurring: isRecurring
+    })
     setIsValidBudget(true)
+  }
+
+const addBudget = async (budgetData) => {
+  try {
+    const newBudget = await axios.post(`${API_BASE_URL}/budgets`, budgetData)
+    localStorage.setItem("budgetId", newBudget.data?.id)
+  } catch (error) {
+    console.error('Error adding budget', error)
+  }
 }
 
   return (
     <div className="contenedor-presupuesto contenedor sombra">
       <form  onSubmit={handleBudget} className="formulario">
+      <h1> Create a new Budget</h1>
+        <div className="campo">
+          <label>Budget Name</label>
+          <input
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Add a name for your budget"
+            value={budgetName}
+            onChange={e => setbudgetName(e.target.value)}
+          />
+        </div>
         <div className="campo">
           <label>Define Budget</label>
           <input 
@@ -33,7 +68,19 @@ const NewBudget = ({
             onChange={ e => setBudget(Number(e.target.value))}
             />
         </div>
-        <input type="submit" value='Add' />
+        <div className="campo checkbox-container">
+          <label className="checkbox-label">
+            Recurring
+            <input
+              className="checkbox-input"
+              type="checkbox"
+              checked={isRecurring}
+              onChange={() => setIsRecurring(!isRecurring)}
+            />
+            <span className="checkbox-custom"></span>
+          </label>
+        </div>
+        <input type="submit" value='Add'  />
         {message && <Message type="error">{message}</Message>}
       </form>
     </div>
